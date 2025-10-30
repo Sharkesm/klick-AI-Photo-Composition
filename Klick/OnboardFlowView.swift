76684bad4c2f22e668736ca07148f9d7f094a3d0
Sharkes Monken <16168370+Sharkesm@users.noreply.github.com>
2025-10-30 14:50:19 +0800

    [CHORE] Add onboarding flow

---
+++ Klick/OnboardFlowView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardFlowView.swift
+//  Klick
+//
+//  Created by Manase on 19/10/2025.
+//
+
+import SwiftUI
+
+struct OnboardFlowView: View {
+    var body: some View {
+        Text("OnboardView")
+    }
+}
+
+#Preview {
+    
+}
